d0647efa181e468f19c37f6ecaf6b26aeefeda2d
Geoffrey Lisch <>
2021-11-20 10:21:46 -0700

    Modal changes

---
+++ main.js
@@ -0,0 +1,18 @@
+$(function (){
+    $('#loginButton').on('click',function(){
+        $('#loginModal').modal('show');
+    });
+
+    $('#beanOneButton').on('click',function(){
+        $('#beanOneModal').modal('show');
+    });
+
+    $('#beanTwoButton').on('click',function(){
+        $('#beanTwoModal').modal('show');
+    });
+
+    $('#beanThreeButton').on('click',function(){
+        $('#beanThreeModal').modal('show');
+    });
+
+});
